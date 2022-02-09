7502d82806c7fc70431e48fd97507980f07c151b
Sheharyar10 <sheharyar.naeem@servixon.com>
2022-02-09 18:46:37 +0500

    create commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div className='header_section'>Header</div>
+    </>
+  )
+}
+
+export default Header
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div className='home_section'>Home</div>
+    </>
+  )
+}
+
+export default Home
